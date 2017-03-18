c37bd3cfb009061e54873212bda41d2904c4d9fe
Dan Oneata <dan.oneata@gmail.com>
2017-03-18 19:11:48 +0200

    MINOR Fix variable name

--- giggle/data.py
+++ giggle/data.py
@@ -93,12 +93,12 @@ def pick_from_random_users(data_frame: DataFrame, nr_users: int) -> DataFrame:
     return data_frame.reset_index(drop=True, inplace=False)
 
 
-pick_from_300_random_users = lambda d: pick_from_random_users(d, nr_users=1500)
+pick_from_1500_random_users = lambda d: pick_from_random_users(d, nr_users=1500)
 
 
 DATASETS = {
     'large': lambda: Dataset(nr_folds=3),
-    'small': lambda: Dataset(nr_folds=3, subsample=pick_from_300_random_users),
+    'small': lambda: Dataset(nr_folds=3, subsample=pick_from_1500_random_users),
 }
 
 
